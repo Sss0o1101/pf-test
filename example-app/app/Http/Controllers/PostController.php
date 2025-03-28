95f9ed7ab921141cbe7e8e6d52d29637db8a0c36
alias <matusoyo2436@gmail.com>
2025-03-28 23:52:44 +0900

    データベース導入による修正

--- example-app/app/Http/Controllers/PostController.php
+++ example-app/app/Http/Controllers/PostController.php
@@ -3,28 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
 
-    private $posts = [
-        'Title 0',
-        'Title 1',
-        'Title 2',
-    ];
+    // private $posts = [
+    //     'Title 0',
+    //     'Title 1',
+    //     'Title 2',
+    // ];
 
     public function index() {
-
-        return view('index')->with(['posts' => $this->posts]);
+        // $posts = Post::all();
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();  //created_at でソートして、降順で取得する
+        return view('index')->with(['posts' => $posts]);
     }
 
-    public function show($id) {
-        $posts = [
-            'Title 0',
-            'Title 1',
-            'Title 2',
-        ];
+    public function show(Post $post) {   //Implicit Binding は、URL の一部をパラメータ化している
 
-        return view('posts.show')->with(['post' => $posts[$id]]);  //posts フォルダの show ビューを表示する
+        // $posts = [
+        //     'Title 0',
+        //     'Title 1',
+        //     'Title 2',
+        // ];
+
+        // $post = Post::find($id);
+        //$post = Post::findOrFail($id);                       //findOrFail は、見つからない場合は 404 エラーを返す
+        return view('posts.show')->with(['post' => $post]);  //posts フォルダの show ビューを表示する
+
     }
 }
