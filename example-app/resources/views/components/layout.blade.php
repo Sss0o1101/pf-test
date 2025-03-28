a2efe66fdc55f5d4397f22d18d3d461a60731f1b
alias <matusoyo2436@gmail.com>
2025-03-28 15:42:20 +0900

    <title>{{ $title }}</title>

--- example-app/resources/views/components/layout.blade.php
+++ example-app/resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>{{ $title }}</title>
     <link rel="stylesheet" href="{{ url('style.css') }}">
 </head>
 <body>
