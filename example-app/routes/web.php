065a0ab7021f6cea513041db6e3248d206aee964
alias <matusoyo2436@gmail.com>
2025-03-28 14:40:24 +0900

    added new text

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\PostController;
 
 // Route::get('/', ['App\Http\Controllers\PostController','index'] );
 //Route::get('/', [App\Http\Controllers\PostController::class,'index']);
-Route::get('/', [PostController::class, 'index']);
+Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
 //URL の一部をパラメータ化
 //{id} は、URL の一部をパラメータ化している。
-Route::get('/posts/{id}', [PostController::class, 'show']);  // メソッド名は、記事の詳細を表示する、という意味で、show という名前をつけている
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show')  ;  // メソッド名は、記事の詳細を表示する、という意味で、show という名前をつけている
