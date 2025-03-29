b434d84c717793752b2b4d19c3f04c378a16d0aa
alias <matusoyo2436@gmail.com>
2025-03-29 22:33:39 +0900

    added new text

--- example-app/resources/views/posts/show.blade.php
+++ example-app/resources/views/posts/show.blade.php
@@ -6,19 +6,41 @@
        {{ $post->title }} | My latest App
     </x-slot:title>
 
-    <h1>{{ $post->title }}</h1>
+    <h1>
+        {{ $post->title }}
         {{-- <p class="back-link"><a href="/">Back</a></p> --}}
         {{-- <p class="back-link"><a href="/">Back</a></p> --}}
         <a href="{{ route('posts.edit', $post) }}">Edit</a>
-        <form action="{{ route('posts.destroy', $post) }}" method="post">
-            @method('delete')
+        <form method="post" action="{{ route('posts.destroy', $post) }}" id="delete-form">
+            @method('DELETE')
             @csrf
             <button>Delete</button>
         </form>
+    </h1>
     <p>{!! nl2br(e($post->body)) !!}</p>
 
 
     <p class="back-link"><a href="{{ route('posts.index')}}">Back</a></p>
+
+    <script>
+        'use strict';
+        // const deleteForm = document.getElementById('delete-form');
+        // deleteForm.addEventListener('submit', function(e) {
+        //     e.preventDefault();
+        //     if (confirm('Are you sure you want to delete this post?')) {
+        //         this.submit();
+        //     }
+        // });
+        const form = document.querySelector('#delete-form');
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+            if (confirm('Sure?') === false) {
+                return;
+            }
+
+            form.submit();
+        });
+    </script>
 
 </x-layout>
 
