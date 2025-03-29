2bae89e689e6ffe2529bfd1cc0910aa87996874d
alias <matusoyo2436@gmail.com>
2025-03-29 13:54:25 +0900

    -Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show')  ;  // メソッド名は、記事の詳細を表示する、という意味で、show という名前をつけている +//Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show')  ;  // メソッド名は、記事の詳細を表示する、という意味で、show という名前をつけている + +//Route::resource は、リソースコントローラーを定義する。 +//except は、index メソッドを除外する。 +Route::resource('posts', PostController::class)->except(['index']);

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -9,4 +9,8 @@ Route::get('/', [PostController::class, 'index'])->name('posts.index');
 
 //URL の一部をパラメータ化
 //{id} は、URL の一部をパラメータ化している。
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show')  ;  // メソッド名は、記事の詳細を表示する、という意味で、show という名前をつけている
+//Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show')  ;  // メソッド名は、記事の詳細を表示する、という意味で、show という名前をつけている
+
+//Route::resource は、リソースコントローラーを定義する。
+//except は、index メソッドを除外する。
+Route::resource('posts', PostController::class)->except(['index']);
