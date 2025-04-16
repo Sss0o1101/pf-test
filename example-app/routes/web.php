1b000122c679fb0437c80cd20a09d543f6007e3e
alias <matusoyo2436@gmail.com>
2025-04-16 22:12:36 +0900

    added new text

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,10 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CommentController;
 
 // Route::get('/', ['App\Http\Controllers\PostController','index'] );
 //Route::get('/', [App\Http\Controllers\PostController::class,'index']);
-Route::get('/', [PostController::class, 'index'])->name('posts.index');
+
+Route::get('/', [PostController::class, 'index'])->name('posts.index');  // 名前付きルーティング
 
 //URL の一部をパラメータ化
 //{id} は、URL の一部をパラメータ化している。
@@ -15,3 +17,6 @@ Route::get('/', [PostController::class, 'index'])->name('posts.index');
 //except は、index メソッドを除外する。
 Route::resource('posts', PostController::class)->except(['index']);
 
+// コメントを追加するためのルーティング
+//Route::post('/posts/{post}/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::resource('posts.comments', CommentController::class)->only(['store', 'destroy']);
