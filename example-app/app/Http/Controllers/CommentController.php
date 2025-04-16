cb73e91672f1906f9f8ab3d2308a1dfb328c572c
alias <matusoyo2436@gmail.com>
2025-04-16 22:12:17 +0900

    added new text

--- example-app/app/Http/Controllers/CommentController.php
+++ example-app/app/Http/Controllers/CommentController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Comment;
+use App\Models\Post;
 
 class CommentController extends Controller
 {
-    //
+    /**
+     * コメントを保存
+     */
+    public function store(Request $request, Post $post)
+    {
+        $request->validate([
+            'body' => 'required',
+        ]);
+
+        $comment = new Comment();
+        $comment->body = $request->body;
+        $comment->post_id = $post->id;
+        $comment->save();
+
+        return redirect()->route('posts.show', $post);
+    }
 }
