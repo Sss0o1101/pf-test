b49d23e26b370b4e314f137c4d2e6bf673904ade
alias <matusoyo2436@gmail.com>
2026-08-29 22:24:49 +0900

    no message

--- example-app/app/Http/Controllers/CommentController.php
+++ example-app/app/Http/Controllers/CommentController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Comment;
-use App\Models\Post;
+use App\Models\Comment;    //Comment モデルをインポートする
+use App\Models\Post;       //Post モデルをインポートする
 
 class CommentController extends Controller
 {
